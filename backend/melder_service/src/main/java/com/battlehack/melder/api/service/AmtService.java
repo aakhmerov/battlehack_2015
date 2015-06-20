a8d0c579229c3995a5ad84a0174fa81b62915013
Askar Akhmerov <askar.akhmerov@smava.de>
2015-06-20 15:29:21 +0200

    add loading of services from cache

--- backend/melder_service/src/main/java/com/battlehack/melder/api/service/AmtService.java
+++ backend/melder_service/src/main/java/com/battlehack/melder/api/service/AmtService.java
@@ -1,7 +1,9 @@
 package com.battlehack.melder.api.service;
 
+import com.battlehack.melder.api.tos.PossibleBookingsTO;
 import com.battlehack.melder.api.tos.ServiceTO;
 import com.battlehack.melder.api.tos.ServicesTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -11,7 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.List;
+import java.io.InputStream;
 
 /**
  * Created by aakhmerov on 20.06.15.
@@ -21,6 +23,7 @@ public class AmtService {
     private static final Logger LOGGER = LoggerFactory.getLogger(AmtService.class);
     private static final String BASE_URL = "http://service.berlin.de";
     private static final String LIST_URL = BASE_URL + "/dienstleistungen/";
+    private static final String CACHED_SERVICES = "services.json";
 
     /**
      * Connect to the website providing list of available services
@@ -69,5 +72,39 @@ public class AmtService {
         }
 
         return filteredServices;
+    }
+
+    /**
+     * Utility method to load services from file cached on the FS, not to
+     * overload our precious government portal.
+     *
+     * @return wrapped object representing all filtered services
+     */
+    public ServicesTO loadCachedServices () {
+        ObjectMapper mapper = new ObjectMapper();
+        InputStream json = this.getClass().getClassLoader().getResourceAsStream(CACHED_SERVICES);
+        ServicesTO cachedServices = null;
+        try {
+             cachedServices = mapper.readValue(json,ServicesTO.class);
+        } catch (IOException e) {
+            LOGGER.error("can't parse cached values");
+        }
+        return cachedServices;
+    }
+
+    /**
+     * Obtain list of bookable appointments for specified service
+     * @param service - service descriptor that has to be inspected for booking possibilities
+     *                bookingUrl is expected to be present and not null
+     * @return
+     */
+    public PossibleBookingsTO getPossibleBookings(ServiceTO service) {
+        PossibleBookingsTO result = new PossibleBookingsTO();
+        try {
+            Document doc = Jsoup.connect(BASE_URL + service.getBookingUrl()).get();
+        } catch (IOException e) {
+            LOGGER.error("can't get booking calendar", e);
+        }
+        return result;
     }
 }
---
+++ backend/melder_service/src/main/java/com/battlehack/melder/api/tos/PossibleBookingTO.java
@@ -0,0 +1,25 @@
+package com.battlehack.melder.api.tos;
+
+/**
+ * Created by aakhmerov on 20.06.15.
+ */
+public class PossibleBookingTO {
+    private String date;
+    private String place;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+}
