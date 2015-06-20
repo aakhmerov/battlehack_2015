0b565c801e62a3906680654efe6ee9e0d5cf0ecd
Askar Akhmerov <askar.akhmerov@smava.de>
2015-06-20 20:15:14 +0200

    add endpoint with serialized user datas

--- backend/melder_rest/src/main/java/com/battlehack/melder/api/rest/MelderRestService.java
+++ backend/melder_rest/src/main/java/com/battlehack/melder/api/rest/MelderRestService.java
@@ -4,6 +4,11 @@ import com.battlehack.melder.api.domain.entities.Status;
 import com.battlehack.melder.api.service.AmtService;
 import com.battlehack.melder.api.tos.PossibleBookingsTO;
 import com.battlehack.melder.api.tos.ServicesTO;
+import com.battlehack.melder.api.tos.UserDataTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -16,6 +21,7 @@ import javax.ws.rs.core.MediaType;
 @Path("/amt")
 @Component
 public class MelderRestService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MelderRestService.class);
 
     @Autowired
     private AmtService amtService;
@@ -39,6 +45,21 @@ public class MelderRestService {
     @Path("/bookings/{serviceId}")
     public PossibleBookingsTO getBookings(@PathParam("serviceId") String serviceId) {
         PossibleBookingsTO result = this.amtService.getBookings(serviceId);
+        return result;
+    }
+
+    @POST
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    @Path("/bookings")
+    public PossibleBookingsTO getBookings(UserDataTO userDataTO) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            LOGGER.debug(mapper.writeValueAsString(userDataTO));
+        } catch (JsonProcessingException e) {
+            LOGGER.error("cant serialize user",e);
+        }
+        PossibleBookingsTO result = this.amtService.getBookings(userDataTO.getServiceId());
         return result;
     }
 }
---
+++ backend/melder_service/src/main/java/com/battlehack/melder/api/tos/UserDataTO.java
@@ -0,0 +1,92 @@
+package com.battlehack.melder.api.tos;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+/**
+ * Created by aakhmerov on 20.06.15.
+ *
+ *   date: ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday"],
+ email: "hans@wurst.us",
+ firstname: "Hans",
+ lastname: "Wurst",
+ phone: "0133713371337",
+ time: "10,18,
+ zipcodes: ["12098", "12239", "13337"]
+
+ */
+@XmlRootElement
+public class UserDataTO {
+    private String email;
+    private String lastName;
+    private String firstName;
+    private String phone;
+    private String time;
+    private List<String> zipCodes;
+    private List <String> days;
+    private String serviceId;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public List<String> getZipCodes() {
+        return zipCodes;
+    }
+
+    public void setZipCodes(List<String> zipCodes) {
+        this.zipCodes = zipCodes;
+    }
+
+    public List<String> getDays() {
+        return days;
+    }
+
+    public void setDays(List<String> days) {
+        this.days = days;
+    }
+
+    public String getServiceId() {
+        return serviceId;
+    }
+
+    public void setServiceId(String serviceId) {
+        this.serviceId = serviceId;
+    }
+}
