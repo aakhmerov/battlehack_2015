61ba12bee3f49c165b10aca09a59b91164c5b417
Luketep <philiphoedtke@googlemail.com>
2015-06-21 07:31:25 +0200

    added booking model  enabled booking from the client

--- frontend/public/js/views/ResultView.js
+++ frontend/public/js/views/ResultView.js
@@ -1,3 +1,4 @@
+/* global . */
 define([
 	'jquery',
 	'underscore',
@@ -5,6 +6,7 @@ define([
 	'Mustache',
 	'views/AbstractView',
 	'models/ResultModel',
+	'models/BookingModel',
 	'text!/templates/result_view.html',
 	'text!/templates/result_table_row.html'
 ], function DefineResultView(
@@ -14,12 +16,15 @@ define([
 	Mustache,
 	AbstractView,
 	ResultModel,
+	BookingModel,
 	templateTable,
 	templateTableRow
 ) {
 	return AbstractView.extend({
 		el: '#container',
-		events: {},
+		events: {
+			'click .book': 'doBooking'
+		},
 		name: 'ResultView',
 		initialize: function(options) {
 			console.log(this.name + ': init');
@@ -31,7 +36,8 @@ define([
 			this.results = [];
 			this.options = JSON.parse(options.response);
 			
-			this.model = new ResultModel();
+			this.resultModel = new ResultModel();
+			this.bookingModel = new BookingModel();
 			
 			this.interval = window.setInterval(this.fetch.bind(this), this.pollingInterval);
 		},
@@ -47,7 +53,8 @@ define([
 					address: result.placeAddress,
 					date: result.date,
 					time: result.bookingTime,
-					bookingUrl: result.bookingUrl
+					bookingUrl: result.bookingUrl,
+					dateUrl: result.dateUrl
 				});
 			}, this);
 
@@ -64,7 +71,7 @@ define([
 		fetch: function() {
 			this.currentTry++;
 			console.log(this.name + ': Saveing data... %d try', this.currentTry);
-			this.model.save(this.options, {
+			this.resultModel.save(this.options, {
 				success: this.fetchSuccess.bind(this),
 				error: this.fetchError.bind(this)
 			});
@@ -84,6 +91,35 @@ define([
 				console.warn(this.name + ': Stop polling after %d fails!', this.maxTries);
 				window.clearInterval(this.interval);
 			}
+		},
+		doBooking: function(e) {
+			console.log(this.name + ': doBooking!');
+			var booking = e.target,
+				bookingData = {
+					user: this.options,
+					desiredBooking: {
+						bookingTime: booking.dataset.bookingTime,
+						bookingUrl: booking.dataset.bookingUrl,
+						date: booking.dataset.date,
+						dateUrl: booking.dataset.dateUrl,
+						placeAddress: booking.dataset.placeAddress,
+						placeName: booking.dataset.placeName 
+					}
+				};
+			
+			console.log(this.name + ': BookingData >', bookingData);
+			
+			this.bookingModel.save(bookingData, {
+				success: this.bookingSuccess.bind(this),
+				error: this.bookingError.bind(this)
+			});			
+		},
+		bookingSuccess: function(response) {
+			console.log(this.name + ': bookingSuccess!', response);
+		},
+		bookingError: function(response) {
+			console.log(this.name + ': bookingError!', response);
 		}
+		
 	});
 });
